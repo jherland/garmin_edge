7c7134a7b3f7e7a91b61da5df0bd4883a645afb5
Johan Herland <johan@herland.net>
2013-10-20 22:30:28 +0200

    Fix timezone issue when parsing .fit files
    The .fit file contains no timezone information. Therefore allow a timezone
    offset to be specified to the FitParser constructor. This offset will be
    applied to all timestamps read from the .fit file in order to convert the
    parsed timestamps into a Unix timestamp (seconds since _UTC_ epoch).
    
    If no timezone offset is supplied, then derive the timezone offset as
    follows: Assume the .fit file timestamps are recorded in the same time
    zone as the current local time zone, and then apply the timezone offset
    that were in effect at the time the .fit file were recorded (taking DST
    into account).

--- fit_parser.py
+++ fit_parser.py
@@ -14,9 +14,24 @@ def semicircles_to_radians(semicircles):
     """Convert integer value of semicircles to radians."""
     return float(semicircles) * math.pi / 2**31
 
+def local_timezone_offset(secs):
+    """Return the timezone offset of the local timezone at the given time.
+
+    The returned value is the number of seconds to be added to UTC time to get
+    the corresponding local time at the given point in time.
+    """
+    local, utc = list(time.localtime(secs)), list(time.gmtime(secs))
+    local[8], utc[8] = 0, 0
+    return time.mktime(local) - time.mktime(utc)
+
 class FitParser(object):
-    @staticmethod
-    def parseFitRecord(r, last_sample):
+    def __init__(self, path, tz_offset=None):
+        self.path = path
+        self.tz_offset = tz_offset
+        self.act = Activity(path)
+        self.act.parse()
+
+    def parseFitRecord(self, r, last_sample):
         d = r.as_dict()
         try:
             lat = semicircles_to_radians(d['position_lat'])
@@ -26,10 +41,11 @@ class FitParser(object):
             lon = semicircles_to_radians(d['position_long'])
         except KeyError:
             lon = last_sample.lon
-        return ActSample(
-            time.mktime(d['timestamp'].timetuple()),
-            lat,
-            lon,
+        ts = time.mktime(d['timestamp'].timetuple())
+        if self.tz_offset is None:
+            self.tz_offset = local_timezone_offset(ts)
+        ts -= self.tz_offset
+        return ActSample(ts, lat, lon,
             d.get('altitude', last_sample.alt),
             d.get('temperature', last_sample.temp),
             d.get('distance', last_sample.d),
@@ -37,11 +53,6 @@ class FitParser(object):
             d.get('heart_rate', last_sample.hr),
             d.get('cadence', last_sample.cad),
         )
-
-    def __init__(self, path):
-        self.path = path
-        self.act = Activity(path)
-        self.act.parse()
 
     def samples(self):
         """Yield all trackpoints as ActSample objects."""
