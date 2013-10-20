6074ca279e2da2c913bd18e82d613ffb88f3e3f8
Johan Herland <johan@herland.net>
2013-10-21 00:01:15 +0200

    Add common frontend for FitParser and TcxParser

---
+++ act_parser.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from fit_parser import FitParser
+from tcx_parser import TcxParser
+
+class ActParser(object):
+    """Frontend for parsers generating ActSamples."""
+    def __init__(self, path):
+        if path.endswith(".fit"):
+            self.p = FitParser(path)
+        elif path.endswith(".tcx"):
+            self.p = TcxParser(path)
+        else:
+            raise ValueError("Don't know which parser to use for '%s'" % (path))
+
+    def __getattr__(self, name):
+        return getattr(self.p, name)
+
+def main(path):
+    print "Reading activity data from %s..." % (path),
+    ap = ActParser(path)
+    print "done"
+    for n, s in enumerate(ap.samples()):
+        print "%6d: %s" % (n, s)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(*sys.argv[1:]))
