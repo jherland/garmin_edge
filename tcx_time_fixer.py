9ebac77f871a9f16ba334ae9f3598742f7afc70e
Johan Herland <johan@herland.net>
2015-08-16 20:52:35 +0200

    Add script for tweaking .tcx timestamps according to a spec given on the command line.

---
+++ tcx_time_fixer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import re
+
+
+TimePattern = re.compile(r'\s*<Time>\s*(.*)\s*</Time>\s*\n')
+
+
+def parse_arg(arg):
+    t_from, rest = arg.split('#')
+    t_0, t_1 = rest.split('>')
+    t_from = datetime.strptime(t_from, '%Y-%m-%dT%H:%M:%S')
+    t_0 = datetime.strptime(t_0, '%Y-%m-%dT%H:%M:%S')
+    t_1 = datetime.strptime(t_1, '%Y-%m-%dT%H:%M:%S')
+    return t_from, t_1 - t_0
+
+
+class TimeMapper:
+    def __init__(self, *maps):
+        self.maps = list(maps)  # list of (datetime, timedelta) pairs
+
+    def map(self, t):
+        assert t >= self.maps[0][0]
+        delta = None
+        for m, d in self.maps:
+            if t < m:
+                break
+            delta = d
+        assert delta is not None
+        return t + delta
+
+    def __call__(self, match):
+        t = datetime.strptime(match.group(1), '%Y-%m-%dT%H:%M:%S.000Z')
+        t = self.map(t)
+        i, j = match.span(1)
+        line = match.group(0)
+        return line[:i] + t.strftime('%Y-%m-%dT%H:%M:%S.000Z') + line[j:]
+
+
+def main(in_f, out_f, *args):
+    maptime = TimeMapper(*map(parse_arg, args))
+
+    for line in in_f:
+        out_f.write(TimePattern.sub(maptime, line))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.stdin, sys.stdout, *sys.argv[1:]))
