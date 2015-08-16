3329c256e53bfa4677197e605c850e6f802d6c00
Johan Herland <johan@herland.net>
2015-08-16 20:39:37 +0200

    Add .gpx support to act_parser.py

--- act_parser.py
+++ act_parser.py
@@ -3,6 +3,7 @@
 
 from fit_parser import FitParser
 from tcx_parser import TcxParser
+from gpx_parser import GpxParser
 
 class ActParser(object):
     """Frontend for parsers generating ActSamples."""
@@ -11,6 +12,8 @@ class ActParser(object):
             self.p = FitParser(path)
         elif path.endswith(".tcx"):
             self.p = TcxParser(path)
+        elif path.endswith(".gpx"):
+            self.p = GpxParser(path)
         else:
             raise ValueError("Don't know which parser to use for '%s'" % (path))
 
